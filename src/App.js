1106cbf37e8d3d28ad03b479b95158aa3c75c670
Giuseppe Falcidia <giuseppefalcidia.mail@gmail.com>
2022-04-01 07:39:44 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+// global access
+const PayPalButton = window.paypal.Buttons.driver("react", {React, ReactDOM});
+
+function App() {
+  const [price, setPrice] = useState(0);
+ const createOrder = (data, actions) => {
+    return actions.order.create ({
+      purchase_units: [
+        {
+          amount: {
+            value: price,
+          },
+        },
+      ],
+    });
+  }
+
+  const onApprove = (data, actions) => {
+    console.log(data);
+    return actions.order.capture();
+  }
+
+  return (
+    <div className="App">
+    <div className="wrapper">
+     <input type="number" 
+     onChange={e => setPrice(e.target.value)}  
+     value={price}
+     placeholder="Select your amount" />
+      <PayPalButton 
+      createOrder={(data, actions) => createOrder(data, actions)}
+      onApprove={(data, actions) => onApprove(data, actions)}
+      />
+    </div>
+    </div>
+  );
+}
+
+export default App;
